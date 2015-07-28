9d55fe34201a8e1c2f0ec2cc6bb750f4c10c1da2
chen zefan <czfshine@gmail.com>
2015-07-28 19:38:24 +0800

    修复Full中列表不连续的bug。

--- Project_1_Instant_Markup/full/rules.py
+++ Project_1_Instant_Markup/full/rules.py
@@ -39,9 +39,10 @@ class ListItemRule(Rule):
     def condition(self, block):
         return block[0] == '-'
     def action(self, block, handler):
-        handler.start(self.type)
-        handler.feed(block[1:].strip())
-        handler.end(self.type)
+        for item in block.split("\n"):
+            handler.start(self.type)
+            handler.feed(item[1:].strip())
+            handler.end(self.type)
         return True
 class ListRule(ListItemRule):
     """
