d0f52156b4c63dfb19e4f1cf9383f4a20c37c842
chen zefan <czfshine@gmail.com>
2015-07-28 18:09:54 +0800

    完整实现(full)

---
+++ Project_1_Instant_Markup/full/markup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import sys, re
+sys.path.append("../")
+from handlers import *
+from block import *
+
+from rules import *
+class Parser:
+    """
+    A Parser reads a text file, applying rules and controlling a
+    handler.
+    """
+    def __init__(self, handler):
+        self.handler = handler
+        self.rules = []
+        self.filters = []
+    def addRule(self, rule):
+        self.rules.append(rule)
+    def addFilter(self, pattern, name):
+        def filter(block, handler):
+            return re.sub(pattern, handler.sub(name), block)
+        self.filters.append(filter)
+        
+    def parse(self, blocks):
+        self.handler.start('document')
+        for block in blocks:
+            for filter in self.filters:
+                block = filter(block, self.handler)
+            for rule in self.rules:
+                if rule.condition(block):
+                    last = rule.action(block, self.handler)
+                    if last: break
+        self.handler.end('document')
+        
+class BasicTextParser(Parser):
+    """
+    A specific Parser that adds rules and filters in its
+    constructor.
+    """
+    def __init__(self, handler):
+        Parser.__init__(self, handler)
+        self.addRule(ListRule())
+        self.addRule(ListItemRule())
+        self.addRule(TitleRule())
+        self.addRule(HeadingRule())
+        self.addRule(ParagraphRule())
+        self.addFilter(r'\*(.+?)\*', 'emphasis')
+        self.addFilter(r'(http://[\.a-zA-Z/]+)', 'url')
+        self.addFilter(r'([\.a-zA-Z]+@[\.a-zA-Z]+[a-zA-Z]+)', 'mail')
+        
+
+if __name__ == '__main__':
+    #打开测试文件
+    infile=open("../input.txt","r")
+    Blocks=[s for s in blocks(infile)]
+    handler = HTMLRenderer()
+    parser = BasicTextParser(handler)
+    parser.parse(Blocks)
+
